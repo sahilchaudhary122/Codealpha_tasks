40f92d84812752f5b940872a0d9088d163bd191f
Sahil Chaudhary <sahilchaudharygkc@gmail.com>
2026-06-04 09:26:50 +0545

    Added Task 2 SQL Injection Protection System

--- Task_2_SQL_Injection_Protection/models.py
+++ Task_2_SQL_Injection_Protection/models.py
@@ -8,28 +8,22 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 class User(Base):
-
     __tablename__ = "users"
-
     id = Column(
         Integer,
         primary_key=True,
         index=True
     )
-
     username = Column(
         String(100)
     )
-
     email = Column(
         String(100),
         unique=True
     )
-
     encrypted_password = Column(
         String(500)
     )
-
     created_at = Column(
         TIMESTAMP,
         server_default=func.now()
