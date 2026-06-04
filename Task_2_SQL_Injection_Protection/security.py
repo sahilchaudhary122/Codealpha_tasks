79843684a6e0af2a4f0f35d26389fa68e87d4353
Sahil Chaudhary <sahilchaudharygkc@gmail.com>
2026-06-04 09:27:18 +0545

    Added Task 2 SQL Injection Protection System

--- Task_2_SQL_Injection_Protection/security.py
+++ Task_2_SQL_Injection_Protection/security.py
@@ -3,17 +3,13 @@ SECRET_KEY = b"xnSBII6NOEO_VV2oiKXaJjd6J38T6VqELVF12Ae7F5U="
 cipher = Fernet(SECRET_KEY)
 
 def encrypt_data(data):
-
     encrypted = cipher.encrypt(
         data.encode()
     )
-
     return encrypted.decode()
 
 def decrypt_data(data):
-
     decrypted = cipher.decrypt(
         data.encode()
     )
-
     return decrypted.decode()
