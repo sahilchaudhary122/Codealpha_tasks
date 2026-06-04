a40f5e04eba364134eb229d1e4652705de5bc7b9
Sahil Chaudhary <sahilchaudharygkc@gmail.com>
2026-06-04 09:25:28 +0545

    Clean up whitespace in database.py
    Removed unnecessary blank lines in database.py.

--- Task_2_SQL_Injection_Protection/database.py
+++ Task_2_SQL_Injection_Protection/database.py
@@ -1,10 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 DATABASE_URL = "mysql+pymysql://root:@localhost/security_db"
-
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
